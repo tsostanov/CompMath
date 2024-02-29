e75c54e1369e4e1e09d88332abb32599696f6edb
tsostanov <tsostanov@gmail.com>
2024-03-01 01:45:42 +0300

    It's just a little raincloud

--- main.py
+++ main.py
@@ -9,25 +9,52 @@ P3214 367550
 """
 
 def matrix_printer(matrix):
+    max_width = max(len(str(elem)) for row in matrix for elem in row)
     for line in matrix:
-        print(' '.join(line))
+        print(" ".join(str(elem).rjust(max_width) for elem in line))
 
 
-accuracy = int(input('Введите точность:\n'))
-mode = int(input('Выберите режим работы: 1 - ввод с клавиатуры, 2 - работа с файлом\n'))
+accuracy = float(input('Введите точность:\n'))
+
 matrix = []
-if mode == 1:
-    n = int(input('Введите количество строк матрицы:\n'))
-    print('Вводите матрицу:')
-    for i in range(n):
-        new_line = list(map(int, input().split()))
-        matrix.append(new_line)
-elif mode == 2:
-    file = input('Введите название файла с матрицей:\n')
-    with open(file, 'r') as matrix_file:
-        for lines in matrix_file.readlines():
-            matrix.append(lines.split())
+while True:
+    mode = int(input('Выберите режим работы: 1 - ввод с клавиатуры, 2 - работа с файлом\n'))
+    if mode == 1:
+        n = int(input('Введите количество строк матрицы:\n'))
+        print('Вводите матрицу:')
+        for i in range(n):
+            new_line = list(map(float, input().split()))
+            matrix.append(new_line)
+        break
+    elif mode == 2:
+        file = input('Введите название файла с матрицей:\n')
+        try:
+            with open(file, 'r') as matrix_file:
+                for line in matrix_file.readlines():
+                    new_line = list(map(float, line.split()))
+                    matrix.append(new_line)
+            break
+        except FileNotFoundError:
+            print("Файл не найден. Пожалуйста, укажите существующий файл.")
+    else:
+        print("Некорректный режим. Пожалуйста, выберите 1 или 2.")
 
+
+def diagonal_form(matrix):
+    max_index = []
+    for line in matrix:
+        max_line = max(line[:-1])
+        if max_line > (sum(line[:-1]) - max_line):
+            index_line = line.index(max_line)
+            if index_line not in max_index:
+                max_index.append(index_line)
+    if len(max_index) == len(matrix):
+        diagonal_matrix = []
+        for i in max_index:
+            diagonal_matrix.append(matrix[i])
+        return diagonal_matrix
+    else:
+        return 'Матрицу нельзя привести к диагональному преобладанию'
 
 
 correct = method.correction_check(matrix)
@@ -38,11 +65,16 @@ else:
     matrix_printer(matrix)
 
 diagonal = method.diagonal_form(matrix)
-print(diagonal)
 if diagonal == 'Матрицу нельзя привести к диагональному преобладанию':
+    print(diagonal)
     sys.exit(1)
 else:
     matrix = diagonal
-    print('Диагональная форма матрицы: \n')
+    print('Диагональная форма матрицы:')
     matrix_printer(matrix)
 
+equations = method.solve_diagonal_system(matrix)
+print('Вектор неизвестных:')
+matrix_printer(equations)
+# for eq in equations:
+#     print(eq)
--- simple_optimizations.py
+++ simple_optimizations.py
@@ -1,24 +1,44 @@
 def correction_check(matrix):
-    matrix_length = len(matrix[0])
-    for i in range(1, len(matrix)):
-        if len(matrix[i]) != matrix_length:
-            return 'Некорректная матрица'
-    return 'Введеная матрица: \n'
-
+    matrix_length = (len(matrix[0]) - 1)
+    matrix_lines = len(matrix)
+    if matrix_lines == matrix_length:
+        for i in range(1, len(matrix)):
+            if (len(matrix[i]) - 1) != matrix_length:
+                return 'Некорректная матрица'
+        return 'Введеная матрица:'
+    else:
+        return 'Некорректная матрица'
 
 
 def diagonal_form(matrix):
     max_index = []
     for line in matrix:
-        max_line = int(max(line))
-        if max_line >= (sum(line) - max_line):
+        max_line = max(line[:-1])
+        if max_line > (sum(line[:-1]) - max_line):
             index_line = line.index(max_line)
             if index_line not in max_index:
                 max_index.append(index_line)
     if len(max_index) == len(matrix):
-        diagonal_matrix = [[]]
-        for i in max_index:
-            diagonal_matrix.append(matrix[i])
-            return diagonal_matrix
+        diagonal_matrix = [[] for _ in range(len(matrix))]
+        for number, line in enumerate(max_index):
+            diagonal_matrix[line] = matrix[number]
+        return diagonal_matrix
     else:
         return 'Матрицу нельзя привести к диагональному преобладанию'
+
+
+def solve_diagonal_system(matrix):
+    equations = []
+    for i, line in enumerate(matrix):
+        factor = line[i]
+        if factor == 0:
+            break
+        equation = []
+        for j, coefficient in enumerate(line[:-1]):
+            if j != i:
+                equation.append((-1) * coefficient / factor)
+            else:
+                equation.append(0)
+        equation.append(line[-1] / factor)
+        equations.append(equation)
+    return equations
